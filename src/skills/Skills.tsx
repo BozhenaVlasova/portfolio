90bea54aaef24c5e55a9726ed88e0815c1168940
BozhenaVlasova <dix20sept@gmail.com>
2023-01-31 21:20:09 +0300

    added skills block

---
+++ src/skills/Skills.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Skills.module.css'
+import styleContainer from '../common/styles/Container.module.css'
+import Skill from "./skill/Skill";
+
+const Skills = () => {
+    return (
+        <div className={styles.skillsBlock}>
+            <div className={`${styleContainer.container} ${styles.skillsContainer}`}>
+                <h2>My Skills</h2>
+                <div className={styles.skills}>
+                    <Skill title='React' description='Подробное описание навыка'/>
+                    <Skill title='Redux' description='Подробное описание навыка'/>
+                    <Skill title='HTML/CSS' description='Подробное описание навыка'/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
