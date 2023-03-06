66258a67bd0d346cf72fa6490ab55803fbec2412
BozhenaVlasova <dix20sept@gmail.com>
2023-03-06 17:58:21 +0300

    updated Contacts component

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import styles from './Contacts.module.css'
 import styleContainer from '../common/styles/Container.module.css'
 import Title from "../common/components/title/Title";
+import Button from "../common/components/button/Button";
+import letter from "../images/letter.svg"
 
 const Contacts = () => {
     return (
         <div className={styles.contacts} id={'contacts'}>
             <div className={`${styleContainer.container} ${styles.container}`}>
-                <Title name={'Contacts'} />
-                <form className={styles.form} action={'contacts.php'}>
-                    <input placeholder={'ФИО'} name={'fio'} required/>
-                    <input placeholder={'+(___)__ ___-__-__'} name={'telephone'} required/>
-                    <textarea rows={7} cols={5} name={'info'} required/>
-                </form>
-                <button className={styleContainer.button}>Отправить</button>
+                <Title name={'Contacts'}/>
+                <div className={styles.descriptionAndForm}>
+                    <div className={styles.description}>
+                    <span>Do you have suggestions? Write to me, don't be shy :)</span>
+                        <p><img src={letter}/>dix20sept@gmail.com</p>
+                    </div>
+                    <div className={styles.formAndButton}>
+                        {/*<h4>Do you have suggestions? Write to me, don't be shy :)</h4>*/}
+                        <form className={styles.form} action={'contacts.php'}>
+                            <input placeholder={'Name*'} name={'name'} required/>
+                            <input placeholder={'Email*'} name={'email'} required/>
+                            <textarea placeholder={'Your message...*'} rows={7} cols={5} name={'message'} required/>
+                        </form>
+                        <Button name={'Send message'}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
