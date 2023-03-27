b25ca21c16637784fd92bb529ff80c6728d31275
BozhenaVlasova <dix20sept@gmail.com>
2023-03-28 01:45:18 +0300

    updated Header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -6,17 +6,26 @@ import github from '../images/messengers/github.svg';
 import linkedin from '../images/messengers/linkedin.svg';
 import logo from '../images/react.svg';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faContactCard, faDatabase, faFolderOpen, faHomeUser} from '@fortawesome/free-solid-svg-icons'
+import {faContactCard, faDatabase, faFolderOpen, faHomeUser, faUser} from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header}`}>
+            <div className={`${styles.mobHeader} ${styles.menuOpen}`}>
+                <button className={styles.togglerMenuOpen}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
             <div className={styles.h1Top}>
                 <img className={styles.img} src={logo}/>
                 <h4>Bozhena</h4>
             </div>
             <div className={styles.pages}>
                     <a href={'#home'}><FontAwesomeIcon icon={faHomeUser}/><span>Home</span></a>
+                    <a href={'#aboutMe'}><FontAwesomeIcon icon={faUser}/><span>About Me</span></a>
                     <a href={'#skills'} className={styles.active}><FontAwesomeIcon icon={faDatabase}/><span>Skills</span></a>
                     <a href={'#projects'} className={styles.active}><FontAwesomeIcon icon={faFolderOpen}/><span>Projects</span></a>
                     <a href={'#contacts'} className={styles.active}><FontAwesomeIcon icon={faContactCard}/><span>Contacts</span></a>
