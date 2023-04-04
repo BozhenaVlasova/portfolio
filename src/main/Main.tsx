62d71f1f0d6f42f71766b82d88caa586a69ccfaa
BozhenaVlasova <dix20sept@gmail.com>
2023-04-04 23:38:29 +0300

    added animation of text output with cursor

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -3,6 +3,7 @@ import styles from './Main.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 import Button from "../common/components/button/Button";
 import {Fade} from "react-awesome-reveal";
+import ReactTypingEffect from 'react-typing-effect';
 
 const Main = () => {
     return (
@@ -12,7 +13,14 @@ const Main = () => {
                     <div className={styles.text}>
                         <h6>Hello! I am</h6>
                         <h1>Bozhena Vlasova</h1>
-                        <p>I Am Frontend <span className={styles.animated}>Developer</span></p>
+                        <p>I Am <span className={styles.animated}>
+                            <ReactTypingEffect text={"Front-end Developer"}
+                                               speed={200}
+                                               eraseSpeed={200}
+                                               typingDelay={1500}
+                                               eraseDelay={2000}/>
+                        </span></p>
+
                         <Button name={'Dowloand CV'}/>
                     </div>
                 </Fade>
